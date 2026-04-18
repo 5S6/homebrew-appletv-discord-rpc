747331e4b7ef659b268e5933819c0504e187d927
Alek <amacwilliams06@gmail.com>
2026-04-18 01:15:07 -0400

    add appletv-discord-rpc formula

---
+++ Formula/appletv-discord-rpc.rb
@@ -0,0 +1,42 @@
+class AppletvDiscordRpc < Formula
+  desc "Apple TV → Discord Rich Presence"
+  homepage "https://github.com/5S6/appletv-discord-rpc"
+  url "https://github.com/5S6/appletv-discord-rpc/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "4b0a7cf7c6dc3d2dffd41c802f9cc4664687924a0c3463a4a3c661a70992c5f9"
+  license "MIT"
+
+  depends_on "deno"
+  depends_on :macos
+
+  def install
+    libexec.install "appletv_discord.ts"
+
+    (bin/"appletv-discord-rpc").write <<~EOS
+      #!/bin/bash
+      exec deno run \
+        --allow-env \
+        --allow-run \
+        --allow-net \
+        --allow-read \
+        --allow-write \
+        --allow-ffi \
+        --allow-import \
+        --unstable-kv \
+        "#{libexec}/appletv_discord.ts" "$@"
+    EOS
+  end
+
+  service do
+    run [opt_bin/"appletv-discord-rpc"]
+    keep_alive true
+    log_path var/"log/appletv-discord-rpc.log"
+    error_log_path var/"log/appletv-discord-rpc.log"
+  end
+
+  def caveats
+    <<~EOS
+      run it once manually first to set up autostart:
+        appletv-discord-rpc
+    EOS
+  end
+end
